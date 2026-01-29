69ecdaafdc4e27909869df67c2ae652f4f404c4a
frenzywho <amvermagaurav007@gmail.com>
2026-01-29 23:26:28 +0530

    ch11 - mutating data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -143,6 +143,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  console.log(id)
   try {
     const data = await sql<InvoiceForm[]>`
       SELECT
