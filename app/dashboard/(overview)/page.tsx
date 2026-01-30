1846ce2b85ca1e6262cf70bfbd019d28d01cb81d
frenzywho <amvermagaurav007@gmail.com>
2026-01-30 19:47:46 +0530

    ch15 - adding metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,11 @@ import {
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
